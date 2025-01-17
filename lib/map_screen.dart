55ab20fa043d1cc1d0febbe24bbbf359dbdf0e2d
Murat Özdil <akurti@Murat-MacBook-Air.local>
2025-01-17 18:11:05 +0300

    solar app first push

---
+++ lib/map_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  final MapController _mapController = MapController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          FlutterMap(
+            mapController: _mapController,
+            options: MapOptions(
+                initialCenter: LatLng(39.7, 32.8),
+                initialZoom: 13.0,
+                onTap: (TapPosition, LatLng) {}),
+            children: [
+              TileLayer(
+                  urlTemplate:
+                      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  subdomains: ['a', 'b', 'c'])
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
